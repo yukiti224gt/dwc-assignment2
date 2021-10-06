535ea9b1fba9c69b09342463c3f5663602a4a2aa
yukiti224gt <mamekiti084@gmail.com>
2021-10-06 18:54:46 +0000

    [FIN] フロント大まかな形，色

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,7 +15,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     if @book.save
       flash[:notice]  = "Book was successfully created."
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book)
     else
       @books = Book.all
       render :index
@@ -27,10 +27,12 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book)
+    else
+      render :edit
     end
   end
   
